6395445e032e0d2d3f2805bb94e7978a21a4fc4d
CJC <cjc0722hz@gmail.com>
2017-05-21 20:34:11 -0700

    separate tokenizer and input rune stream

---
+++ input_stream.go
@@ -0,0 +1,61 @@
+package main
+
+import (
+	"bufio"
+	"errors"
+	"os"
+	"strconv"
+)
+
+func check(err error) {
+	if err != nil {
+		panic(err)
+	}
+}
+
+type InputStream struct {
+	rd             *bufio.Reader
+	pos, line, col int
+	eof            bool
+	rune_size      int // for testing the eof
+}
+
+/* Open the file and initialize the pos, col, line variables*/
+func (is *InputStream) open_file(fimename string) {
+	f, err := os.Open(fimename)
+	check(err)
+	is.rd = bufio.NewReader(f)
+	is.line = 1
+	is.rune_size = -1 // not eof at the first beginning,
+}
+
+func (is *InputStream) peek() rune {
+	r, size, _ := is.rd.ReadRune() // don't check the eof, is_eof() will handle it
+	is.rune_size = size
+	is.rd.UnreadRune()
+	return r
+}
+
+func (is *InputStream) next() rune {
+	r, size, _ := is.rd.ReadRune() // don't check the eof, is_eof() will handle it
+	is.rune_size = size
+	is.pos++
+
+	char := string(r)
+	if char == "\n" {
+		is.line++
+		is.col = 0
+	} else {
+		is.col++
+	}
+	return r
+}
+
+func (is *InputStream) is_eof() bool {
+	return is.rune_size == 0
+}
+
+func (i *InputStream) err_msg(msg string) {
+	err := errors.New(msg + " at line: " + strconv.Itoa(i.line) + " col: " + strconv.Itoa(i.col))
+	panic(err)
+}
--- tokenizer.go
+++ tokenizer.go
@@ -1,67 +1,11 @@
 package main
 
 import (
-	"bufio"
-	"errors"
 	"fmt"
-	"os"
 	"strconv"
 	"unicode"
 	"unicode/utf8"
 )
-
-type InputStream struct {
-	rd             *bufio.Reader
-	pos, line, col int
-	eof            bool
-	rune_size      int // for testing the eof
-}
-
-func check(err error) {
-	if err != nil {
-		panic(err)
-	}
-}
-
-/* Open the file and initialize the pos, col, line variables*/
-func (is *InputStream) open_file(fimename string) {
-	f, err := os.Open(fimename)
-	check(err)
-	is.rd = bufio.NewReader(f)
-	is.line = 1
-	is.rune_size = -1 // not eof at the first beginning,
-}
-
-func (is *InputStream) peek() rune {
-	r, size, _ := is.rd.ReadRune() // don't check the eof, is_eof() will handle it
-	is.rune_size = size
-	is.rd.UnreadRune()
-	return r
-}
-
-func (is *InputStream) next() rune {
-	r, size, _ := is.rd.ReadRune() // don't check the eof, is_eof() will handle it
-	is.rune_size = size
-	is.pos++
-
-	char := string(r)
-	if char == "\n" {
-		is.line++
-		is.col = 0
-	} else {
-		is.col++
-	}
-	return r
-}
-
-func (is *InputStream) is_eof() bool {
-	return is.rune_size == 0
-}
-
-func (i *InputStream) err_msg(msg string) {
-	err := errors.New(msg + " at line: " + strconv.Itoa(i.line) + " col: " + strconv.Itoa(i.col))
-	panic(err)
-}
 
 /*
 	We should define the types of all the token the JSON files can have.
